bfbed159398b91197c3fd2294e3b6afaede769b8
saulotarsobc <saulotarsobc@gmail.com>
2024-12-25 10:54:40 -0300

    adiciona configuração do VSCode, atualiza dependências e implementa fluxo de provedor

---
+++ src/app/components/flows/initial/Sidebar.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { useStore } from "@xyflow/react";
+import { useCallback } from "react";
+
+const transformSelector = (state: any) => state.transform;
+
+export default ({ nodes, setNodes }: any) => {
+  const transform = useStore(transformSelector);
+
+  const selectAll = useCallback(() => {
+    setNodes((nds: any) =>
+      nds.map((node: any) => {
+        return {
+          ...node,
+          selected: true,
+        };
+      })
+    );
+  }, [setNodes]);
+
+  return (
+    <aside>
+      <div className="description">
+        This is an example of how you can access the internal state outside of
+        the ReactFlow component.
+      </div>
+      <div className="title">Zoom & pan transform</div>
+      <div className="transform">
+        [{transform[0].toFixed(2)}, {transform[1].toFixed(2)},{" "}
+        {transform[2].toFixed(2)}]
+      </div>
+      <div className="title">Nodes</div>
+      {nodes.map((node: any) => (
+        <div key={node.id}>
+          Node {node.id} - x: {node.position.x.toFixed(2)}, y:{" "}
+          {node.position.y.toFixed(2)}
+        </div>
+      ))}
+
+      <div className="selectall">
+        <button onClick={selectAll}>select all nodes</button>
+      </div>
+    </aside>
+  );
+};
